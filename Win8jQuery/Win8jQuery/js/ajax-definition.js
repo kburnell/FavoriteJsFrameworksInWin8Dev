d1726dc35c5cf98d4867347455308033f975b2b0
Keith Burnell <kburnell@skylinetechnologies.com>
2013-08-20 08:36:13 -0500

    Initial Commit

---
+++ Win8jQuery/Win8jQuery/js/ajax-definition.js
@@ -0,0 +1,13 @@
+﻿var api_url = "https://api.github.com/users/";
+
+amplify.request.define("github.User", "ajax", {
+    url: api_url + "{id}",
+    type: "GET"
+});
+
+amplify.request.define("github.Repos", "ajax", {
+    url: api_url + "{id}/repos" ,
+    type: "GET"
+});
+
+
---
+++ Win8jQuery/Win8jQuery/js/script.js
@@ -0,0 +1,60 @@
+﻿$(function() {
+    var users = {
+        "User1": { userName: "KBurnell" },
+        "User2": { userName: "CSell5" },
+    };
+
+    populateUsers(users);
+
+    function populateUsers(userList) {
+        $.each(userList, function(i, user) {
+            amplify.request("github.User", { id: user.userName }, function(data) {
+                users[i].profile = data;
+                var li = $("<li></li>", { "data-id": i }).html($("<img></img>", {
+                    src: data.avatar_url + "&s=300",
+                    alt: data.name
+                }));
+                $("#side-bar ul").append(li);
+            });
+        });
+    }
+
+    $("#side-bar").on("click", "li", function(data) {
+        var id = $(this).data("id");
+        $("#side-bar, #dataCanvas").toggle();
+        var user = users[id];
+        tab_profile(user);
+        tab_repos(user);
+        $("#tabs").tabs();
+    });
+
+});
+
+var tab_profile = function (user) {
+    var tab = $("#profile");
+    tab.find("#name").text(user.profile.name);
+    tab.find("#avatar").attr("src", user.profile.avatar_url);
+    tab.find(".tabBackButton").unbind("click").on("click", function () {
+        $("#side-bar, #dataCanvas").toggle();
+    });
+    tab.find("#userName").text(user.profile.login);
+    tab.find("#company").text(user.profile.company);
+};
+
+var tab_repos = function (user) {
+    if (!user.repos) {
+        var tab = $("#repos");
+        var reposList = tab.find("#reposList");
+        reposList.html("");
+        tab.find("#name").text(user.profile.name + "'s Repos");
+        tab.find(".tabBackButton").unbind("click").on("click", function() {
+            $("#side-bar, #dataCanvas").toggle();
+        });
+        amplify.request("github.Repos", { id: user.userName }, function(data) {
+            user.repos = data;
+            $.each(data, function(i, repo) {
+                reposList.append("<li><a class='repoItem' href='" + repo.html_url + "'>" + repo.name + "</a></li>");
+            });
+        });
+    }
+};
